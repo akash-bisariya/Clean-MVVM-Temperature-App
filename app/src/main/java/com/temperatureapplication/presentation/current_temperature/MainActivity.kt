331e954b3dda62526237c56ccf37f8297531ae2e
Akash Bisariya <akashbisariya@Akashs-MacBook-Pro.local>
2023-10-22 16:44:58 +0530

    Initial commit with folder structure

---
+++ app/src/main/java/com/temperatureapplication/presentation/current_temperature/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.temperatureapplication.presentation.current_temperature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.temperatureapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
